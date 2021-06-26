fe23d901548e2721d8a58ab8223afc9ff76de487
Deiver Carrascal <ingendeiver@gmail.com>
2021-06-26 12:55:26 -0500

    push notification chat no db implemented

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ const sendNotifcationsAllUsersExecpNewUser = async (newUserPushId) => {
   const users = await User.find({})
   const tokens = users.map(user => user.push_id).filter(push_id => push_id !== newUserPushId)
 
-  if(tokens.length > 0){
+  if (tokens.length > 0) {
     const message = {
-      notification:{
+      notification: {
         title: 'New user',
         body: 'New user was register in ChatApp,  it!',
         imageUrl: 'https://www.shareicon.net/data/2016/06/30/788859_add_512x512.png'
@@ -34,7 +34,7 @@ const sendNotifcationsAllUsersExecpNewUser = async (newUserPushId) => {
       },
       tokens
     };
-  
+
     admin.messaging().sendMulticast(message)
       .then((response) => {
         console.log(response.successCount + ' messages were sent successfully');
@@ -54,13 +54,23 @@ const createOrUpdateUserWithOauth = (req, res) => {
         console.log("New account");
         // Create it
         new User(req.body).save()
-          .then(_ => {
+          .then(doc => {
             sendNotifcationsAllUsersExecpNewUser(push_id)
-            res.sendStatus(200)
+            res.json({
+              push_id,
+              _id: doc._id
+            })
           })
-          .catch(error => res.status(500).json({ error: error.message }))
+          .catch(error => {
+            console.log(error.message);
+            res.status(500).json({ error: error.message })
+          })
       } else {
         console.log("Exist account");
+        res.json({
+          push_id,
+          _id: result._id
+        })
       }
 
     } else {
@@ -80,13 +90,23 @@ const createOrUpdateUserWithFingerPrintMethod = (req, res) => {
         console.log("New account");
         // Create it
         new User(req.body).save()
-          .then(_ => {
+          .then(doc => {
             sendNotifcationsAllUsersExecpNewUser(push_id)
-            res.sendStatus(200)
+            res.json({
+              push_id,
+              _id: doc._id
+            })
           })
-          .catch(error => res.status(500).json({ error: error.message }))
+          .catch(error => {
+            console.log(error.message);
+            res.status(500).json({ error: error.message })
+          })
       } else {
         console.log("Exist account");
+        res.json({
+          push_id,
+          _id: result._id
+        })
       }
 
     } else {
@@ -99,9 +119,9 @@ app.use(express.json())
 
 app.post('/user', (req, res) => {
   const oauth_id = req.body.oauth_id
-  if(oauth_id){
+  if (oauth_id) {
     createOrUpdateUserWithOauth(req, res)
-  }else{
+  } else {
     createOrUpdateUserWithFingerPrintMethod(req, res)
   }
 
@@ -117,9 +137,33 @@ app.get('/user', async (req, res) => {
 })
 
 app.post('/notification', (req, res) => {
+  console.log("Message: ",req.body);
+  const messages = req.body.messages
   try {
+    const message = {
+      notification: {
+        title: 'New message',
+        body: messages[0].text,
+      },
+      data: {
+        screen: 'Chat',
+        messages: JSON.stringify(messages),
+        fromUser: req.body.from // user own, for answer messages
+      },
+      token: req.body.tokenUserToSend
+    };
 
+    admin.messaging().send(message)
+    .then((response) => {
+      // Response is a message ID string.
+      console.log('Successfully sent message:', response);
+    })
+    .catch((error) => {
+      console.log('Error sending message:', error);
+    });
+  
   } catch (error) {
+    console.log("ERROR-->", error.message);
     res.status(500).json({ error: error.message })
   }
 })
